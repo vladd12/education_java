a7abfed1ad557cc4a3c5236a25fa0e9f562237cc
vladd12 <vladd12z32@gmail.com>
2020-09-22 16:13:15 +0300

    Module 2 updated

--- src/main/java/modules/Module2.java
+++ src/main/java/modules/Module2.java
@@ -4,6 +4,25 @@ package modules;
 
 public class Module2 {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println(repeat("mice",5));
+        System.out.println(repeat("hello",3));
+        System.out.println(repeat("stop",1) + "\n");
 
+        // Задача 2
+
+    }
+
+    // Задача 1
+    public static String repeat(String str, int num) {
+        String repStr = new String("");
+        for (int i = 0; i < str.length(); i++) {
+            int n = num;
+            while(n > 0) {
+                repStr = repStr + str.charAt(i);
+                n = n - 1;
+            }
+        }
+        return repStr;
     }
 }
