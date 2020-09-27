cf24776a0cb57940a4374b63467180cc2a9ce491
vladd12 <vladd12z32@gmail.com>
2020-09-27 13:38:46 +0300

    Module 4 updated. Task 1 completed

--- src/main/java/modules/Module4.java
+++ src/main/java/modules/Module4.java
@@ -7,7 +7,10 @@ public class Module4 {
 
         // Задача 1
         System.out.println("Задача 1.");
+        System.out.println(bessie(10, 7, "hello my name is Bessie and this is my essay"));
 
+        // Задача 2
+        System.out.println("Задача 2.");
 
     }
 
@@ -17,26 +20,31 @@ public class Module4 {
         String result = "";
         int indexStart = 0, indexEnd = 0, k_save = k;
         while (n > 0) { // Пока количество слов не равно нулю
-            for (int i = indexStart; i < str.length() - 1; i++) {
-                if (i > 15) return "Error! A word containing more than 15 characters was found."; // Найдено слово, длина которого больше 15 символов
-                if (str.charAt(i) == ' ') {
+            for (int i = indexStart; i < str.length(); i++) {
+                if (str.charAt(i) == ' ' || i == str.length()-1) {
                     indexEnd = i;
+                    if (i == str.length()-1) indexEnd = indexEnd + 1;
                     break;
                 }
             }
+            if (indexEnd - indexStart > 15) return "Error! A word containing more than 15 characters was found."; // Найдено слово, длина которого больше 15 символов
             String word = str.substring(indexStart, indexEnd);
-            indexStart = indexEnd;
-            if (k_save > word.length() + 1) {
+            indexStart = indexEnd + 1;
+            if (k_save >= word.length()) {
                 result = result + word + " ";
-                k_save = k_save - (word.length() + 1);
+                k_save = k_save - word.length();
             }
             else {
                 k_save = k;
-
+                result = result + "\n" + word + " ";
+                k_save = k_save - word.length();
             }
-
             n = n - 1;
         }
         return result;
     }
+
+    // Задача 2
+
+
 }
