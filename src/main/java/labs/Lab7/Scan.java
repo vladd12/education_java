9a3ebec7c7f076994157ce93b5d00ccbdfb58eab
vladd12 <vladd12z32@gmail.com>
2020-12-01 13:03:55 +0300

    Laba 7 update

--- src/main/java/labs/Lab7/Scan.java
+++ src/main/java/labs/Lab7/Scan.java
@@ -4,11 +4,27 @@ import java.util.*;
 
 public class Scan {
 
+    private static LinkedList<URLDepthPair> CheckedURL = new LinkedList<>();
+    private static LinkedList<URLDepthPair> UncheckedURL = new LinkedList<>();
+
     /**
      * Точка входа программы
      * @param args аргументы командной строки
      */
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // сканер для ввода URL и глубины поиска
+        System.out.print("Input a URL: ");
+        String URL = in.nextLine();
+        System.out.print("Input a depth: ");
+        int depth = in.nextInt();
+        in.close(); // закрываем поток ввода
     }
+
+
+
+
+
+
+
+
 }
--- src/main/java/labs/Lab7/URLDepthPair.java
+++ src/main/java/labs/Lab7/URLDepthPair.java
@@ -1,4 +1,57 @@
 package labs.Lab7;
 
 public class URLDepthPair {
+
+    private String URL; // поле класса со строкой URL
+    private int depth; // поле класса с глубиной поиска
+
+    /**
+     * Конструктор класса по умолчанию
+     */
+    public URLDepthPair() {
+        this.URL = null;
+        this.depth = 0;
+    }
+
+    /**
+     * Конструктор класса с параметрами
+     * @param URL строка URL
+     * @param depth глубина поиска
+     */
+    public URLDepthPair(String URL, int depth) { //
+        this.URL = URL;
+        this.depth = depth;
+    }
+
+    /**
+     * Getter для строки URL
+     * @return текущая строка URL объекта
+     */
+    public String getURL() {
+        return URL;
+    }
+
+    /**
+     * Getter для глубины поиска
+     * @return текущая глубина поиска объекта
+     */
+    public int getDepth() {
+        return depth;
+    }
+
+    /**
+     * Setter для строки URL
+     * @param URL новая строка URL
+     */
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    /**
+     * Setter для глубины поиска
+     * @param depth новая глубина поиска
+     */
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
 }
