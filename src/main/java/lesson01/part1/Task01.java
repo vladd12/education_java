f135fa57289b2ab4255f8b6da57935ed6391e43f
vladd12 <vladd12z32@gmail.com>
2020-12-15 16:57:53 +0300

    Delete useless files from start repo

--- src/main/java/lesson01/part1/Task01.java
+++
@@ -1,7 +0,0 @@
-package lesson01.part1;
-
-public class Task01 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/main/java/lesson01/part1/Task02.java
+++
@@ -1,7 +0,0 @@
-package lesson01.part1;
-
-public class Task02 {
-    public static void main(String[] args) {
-        System.out.println("Hello World! \nHello World! \nHello World!");
-    }
-}
